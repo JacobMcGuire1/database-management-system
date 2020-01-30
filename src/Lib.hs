12ab18e061ce35afcbb74152a2015f0d8d485541
JacobMcGuire1 <mcguirejacob1@gmail.com>
2020-01-30 14:28:10 +0000

    Started to create data types

--- src/Lib.hs
+++ src/Lib.hs
@@ -5,3 +5,29 @@ module Lib
 someFunc :: IO ()
 someFunc = putStrLn "someFunc"
 
+data Database = Database {
+    dbname :: String,
+    tables :: [Table]
+}
+
+data Table = Table {
+    tablename :: String,
+    colheaders :: [ColumnHeader],
+    rows :: [Row]
+}
+
+data ColumnHeader = ColumnHeader {
+    colname :: String,
+    datatype :: String
+}
+
+data DataField = StringField {
+    strdata :: [String]
+    }
+    | IntField {
+    intdata :: Int
+    }
+
+data Row = Row {
+    fields :: [DataField]
+}
